f6998d117f9d3a3740494518400df8390c0b18ab
Daniel Bonhaure <danielbonhaure@gmail.com>
2020-03-23 09:58:18 -0400

    Some improvements

--- shiny.r
+++ shiny.r
@@ -26,59 +26,6 @@ url <- "https://twitter.com/intent/tweet?url=https://danielbonhaure.shinyapps.io
 # save(countries, file="shapeFile.RData")
 load("data/shapeFile.RData")
 
-dataCook<- function(data, countries){
-  
- 
-  data$`Country/Region`<-as.character(data$`Country/Region`)
-  data$`Country/Region`[data$`Country/Region`=="Macau"]<- "Macao"
-  data$`Country/Region`[data$`Country/Region`=="Mainland China"]<- "China"
-  data$`Country/Region`[data$`Country/Region`=="South Korea"]<- "South Korea"
-  data$`Country/Region`[data$`Country/Region`=="North Macedonia"]<- "Macedonia"
-  data$`Country/Region`[data$`Country/Region`=="Czech Republic"]<- "Czechia"
-  data$`Country/Region`[data$`Country/Region`=="Dominican Republic"]<- "Dominican Rep."
-  data$`Country/Region`[data$`Country/Region`=="UK"]<- "United Kingdom"
-  data$`Country/Region`[data$`Country/Region`=="Gibraltar"]<- "United Kingdom"
-  data$`Country/Region`[data$`Country/Region`=="US"]<- "United States"
-  data$`Country/Region`[data$`Country/Region`=="Saint Barthelemy"]<- "St-Barthélemy"
-  
-  data$`Country/Region`[data$`Country/Region`=="Faroe Islands"]<- "Faeroe Is."
-  data$`Country/Region`[data$`Country/Region`=="Bosnia and Herzegovina"]<- "Bosnia and Herz."
-  data$`Country/Region`[data$`Country/Region`=="Vatican City"]<- "Vatican"
-  data$`Country/Region`[data$`Country/Region`=="Korea, South"]<- "South Korea"
-  data$`Country/Region`[data$`Country/Region`=="Republic of Ireland"]<- "Ireland"
-  data$`Country/Region`[data$`Country/Region`=="Taiwan*"]<-"Taiwan"
-  
-  data$`Country/Region`[data$`Country/Region`=="Congo (Kinshasa)"]<-"Congo"
-  data$`Country/Region`[data$`Country/Region`=="Cote d'Ivoire"]<-"Côte d'Ivoire"
-  data$`Country/Region`[data$`Country/Region`=="Reunion"]<-"France"
-  data$`Country/Region`[data$`Country/Region`=="Martinique"]<-"France"
-  data$`Country/Region`[data$`Country/Region`=="French Guiana"]<-"France"
-  data$`Country/Region`[data$`Country/Region`=="Holy See"]<-"Vatican"
-  data$`Country/Region`[data$`Country/Region`=="Cayman Islands"]<-"Cayman Is."
-  data$`Country/Region`[data$`Country/Region`=="Guadeloupe"]<-"France"
-  data$`Country/Region`[data$`Country/Region`=="Antigua and Barbuda"]<-"Antigua and Barb."
-  
-  data$`Country/Region`[data$`Country/Region`=="Curacao"]<-"Curaçao"
-  data$`Country/Region`[data$`Country/Region`=="Guadeloupe"]<-"France"
-  data$`Country/Region`[data$`Country/Region`=="occupied Palestinian territory"]<-"Palestine"
-  data$`Country/Region`[data$`Country/Region`=="Congo (Brazzaville)"]<-"Congo"
-  data$`Country/Region`[data$`Country/Region`=="Equatorial Guinea"]<-"Guinea"
-  data$`Country/Region`[data$`Country/Region`=="Central African Republic"]<-"Central African Rep."
-  
-  data$`Country/Region`[data$`Country/Region`=="Central African Republic"]<-"Central African Rep."
-  data$`Country/Region`[data$`Country/Region`=="Eswatini"]<-"eSwatini"
-  
-  
-
-  # countries$NAME<-as.character(countries$NAME)
-  # countries$NAME[is.na(countries$NAME)]<-"Côte d'Ivoire"
-  data$Pays<-as.character(unique(countries$NAME)[charmatch(data$`Country/Region`,unique(countries$NAME))])
-  print(data$`Country/Region`[is.na(data$Pays)])
-  dataPays<- data%>%dplyr::select(-`Province/State`, -Lat, -Long,-`Country/Region`)%>%group_by(Pays)%>%summarise_each(sum)
-  dataPays$Pays<-as.character(dataPays$Pays)
-  return(dataPays)
-}
-
 
 # ver: https://carto.com/developers/sql-api/reference/#operation/getSQLStatement (para entender format)
 URL <- RCurl::getURL("http://geo.stp.gov.py/user/dgeec/api/v2/sql?q=SELECT%20*%20FROM%20dgeec.paraguay_2002_departamentos&format=GeoJSON")
@@ -439,12 +386,12 @@ server <- function(input, output, session) {
       
     }else{
       if(input$variable %in% c("Total cases", "Total cases/population")){
-        sliderInput("day1", "Day", min(jourDate), max(jourDate),
-                    value =  c(max(jourDate)),animate = T, step = 1
+        sliderInput("day1", "Day", min(jourDate), max(jourDate), 
+                    value =  c(max(jourDate)),animate = T, step = 1, ticks = T
         )
       }else{
-        sliderInput("day2", "Day", min(jourDate), max(jourDate),
-                    value =  c(max(jourDate)-7,max(jourDate)),animate = T, step = 1
+        sliderInput("day2", "Day", min(jourDate), max(jourDate), 
+                    value =  c(max(jourDate)-7,max(jourDate)),animate = T, step = 1, ticks = T
         )
       }
     }
